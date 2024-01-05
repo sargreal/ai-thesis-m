cd077c0ee5b85c96f1122f924c10129b62b6c92a
Felix Haase <felix.haase@stud.uni-bamberg.de>
2024-01-05 15:03:42 +0000

    almost finished anml domain

---
+++ scripts/fape.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fape_version="1.0+66-1f82cbd6+20240105-1406"
+fape_path="/root/master-thesis/fape/planning/target/fape-planning-assembly-${fape_version}.jar"
+
+java -jar $fape_path "$@"
