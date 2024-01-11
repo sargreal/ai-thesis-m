36eb5ec2408670fd0d23942f5e4b349019786d18
Felix Haase <felix.haase@stud.uni-bamberg.de>
2024-01-11 12:09:19 +0000

    first unoptimized working version of the overcooked2 domain

--- scripts/fape.sh
+++ scripts/fape.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-fape_version="1.0+66-1f82cbd6+20240105-1406"
-fape_path="/root/master-thesis/fape/planning/target/fape-planning-assembly-${fape_version}.jar"
+# fape_version="1.0+66-1f82cbd6+20240105-1406"
+# fape_path="/root/master-thesis/fape/planning/target/fape-planning-assembly-${fape_version}.jar"
 
-java -jar $fape_path "$@"
+# java -jar $fape_path "$@"
+
+set +x
+
+cd /root/master-thesis/fape
+
+Command="fape-planning/fgRun -v $@"
+
+sbt --error "$Command"
